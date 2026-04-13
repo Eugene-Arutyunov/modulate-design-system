5621de26ec67a8c164818eece5a2de378bb4a8c9
Kirill Myshkin <kirill@myshkin.eu>
2026-04-14 00:15:46 +0200

    Reorganise /src according to the convention
    - move ui.html and css-bundle.njk to /service
    - move all the prototype page to /prototypes
    - add rewrite rule to 11ty config to strip internal structure
    - move fonts to /assets/service directory

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,11 +2,28 @@ module.exports = function (conf) {
   conf.addFilter("startsWith", (str, prefix) => str.startsWith(prefix));
   conf.addPassthroughCopy("./src/index.js");
   conf.addPassthroughCopy("./src/assets");
-  conf.addPassthroughCopy("./src/fonts");
+  conf.addPassthroughCopy({ "./src/service/fonts": "fonts" });
   conf.addPassthroughCopy("./ui.yaml");
   conf.addPassthroughCopy("./dashboard-charts.json");
 
   conf.addWatchTarget("./src/styles/");
+
+  // remove internal structure
+  conf.addGlobalData("permalink", () => {
+    return (data) => {
+      const stem = data.page.filePathStem;
+      if (stem.startsWith("/service/")) {
+        return stem.slice("/service".length) + "/index.html";
+      }
+      if (stem.startsWith("/prototypes/platform/")) {
+        return stem.slice("/prototypes/platform".length) + "/index.html";
+      }
+      if (stem.startsWith("/prototypes/")) {
+        return stem.slice("/prototypes".length) + "/index.html";
+      }
+      return stem + "/index.html";
+    };
+  });
 
   return {
     dir: {
